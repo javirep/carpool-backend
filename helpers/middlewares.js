b43dc0d54e594c61e790c1cebc1d660351790403
Javier Repetto de los Dolores <javier.repettodelosdolres@gmail.com>
2019-11-19 11:48:31 +0100

    trying to deploy

--- helpers/middlewares.js
+++ helpers/middlewares.js
@@ -2,18 +2,12 @@ const createError = require('http-errors');
 
 exports.isLoggedIn = () => (req, res, next) => {
     if (req.session.currentUser) next();
-    else {
-        console.log("Error. The user is not logged in")
-        next(createError(401))
-    };
+    else next(createError(401));
 };
 
 exports.isNotLoggedIn = () => (req, res, next) => {
     if (!req.session.currentUser) next();
-    else {
-        console.log("Error. The user is already logged in")
-        next(createError(401));
-    }
+    else next(createError(403));
 };
 
 exports.validationLoggin = () => async (req, res, next) => {
